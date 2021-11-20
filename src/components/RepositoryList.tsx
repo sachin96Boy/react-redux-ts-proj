f51debc67322f89c6f7ccf29fcd36759467e0047
M.G.S SUPUNTHAKA <52316373+sachin96Boy@users.noreply.github.com>
2021-11-20 22:42:21 +0530

    fix:completechanges

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+// import { useDispatch } from "react-redux";
+// import { actionCreators } from "../state";
+
+import { useActions } from "../hooks/useActions";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState<string>("");
+  // const dispatch = useDispatch();
+  const { SearchRepositories } = useActions();
+
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.searchReducer
+  );
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // dispatch(actionCreators.SearchRepositories(term));
+    SearchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
-        <button>Search</button>
+      <form onSubmit={onSubmit}>
+        <input
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          type="text"
+        />
+        <button type="submit">Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,2 +1,3 @@
 export * from "./store";
 export * as actionCreators from "./dispatcher/dispatcher";
+export * from "./reducers";
